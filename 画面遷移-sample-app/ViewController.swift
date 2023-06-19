da5f5c40a350641d9ffc2a58351fa70ad402a2f3
KosukeNakagawa <kosukenakagawa730@gmail.com>
2023-06-19 16:47:59 +0900

    Initial Commit

---
+++ 画面遷移-sample-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  画面遷移-sample-app
+//
+//  Created by 中川こうすけ on 2023/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
