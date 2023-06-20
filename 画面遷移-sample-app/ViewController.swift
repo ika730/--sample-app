38e74748e086fe34dd0f3592b564a8d1512a6e33
KosukeNakagawa <kosukenakagawa730@gmail.com>
2023-06-20 12:20:34 +0900

    コードを使わない画面遷移方法を2種類追加

--- 画面遷移-sample-app/ViewController.swift
+++ 画面遷移-sample-app/ViewController.swift
@@ -1,19 +1,37 @@
 //
 //  ViewController.swift
 //  画面遷移-sample-app
-//
-//  Created by 中川こうすけ on 2023/06/19.
+//コードを使った二つの画面遷移方法
+//https://qiita.com/tosh_3/items/e99b77372d8c0f38d98b
+//  
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func seni1(_ sender: UIButton) {
+        
+        counter = counter + 1
+        if counter == 2{
+            self.performSegue(withIdentifier: "toNext", sender: nil)
+            counter = 0
+        }
+    }
+    
+    
+    @IBAction func seni2(_ sender: UIButton) {
+        let storyboard: UIStoryboard = self.storyboard!
+                let second = storyboard.instantiateViewController(withIdentifier: "nextv")
+                self.present(second, animated: true, completion: nil)
+    }
+    
 }
 
